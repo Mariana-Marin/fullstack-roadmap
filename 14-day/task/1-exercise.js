551729fca5a44efd6a8f95e3f9827d836baafb58
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-25 15:46:35 -0500

    Add exercises for sum calculation and number checking

--- 14-day/task/1-exercise.js
+++ 14-day/task/1-exercise.js
@@ -2,3 +2,20 @@
  * Write a JavaScript program to compute the sum of the two given integers.
  * If the two values are the same, then return triple their sum.  
  */
+
+function getSum (numberOne, numberTwo) {
+
+    let sum;
+
+    if (numberOne === numberTwo) {
+        sum = (numberOne+numberTwo)*3;
+        
+    } else { 
+        sum = numberOne+numberTwo;
+    }
+
+    return sum;
+}
+
+console.log(getSum(3, 3));
+console.log(getSum(2, 12));
