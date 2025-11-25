706ecd7d507001801355dbf83b764a48018c9037
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-25 16:37:29 -0500

    Refactor string manipulation functions for consistency and readability

--- 14-day/task/6-exercise.js
+++ 14-day/task/6-exercise.js
@@ -1,3 +1,11 @@
 /**
- * Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.  
+ * Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.
  */
+
+function checkMultiple(number) {
+  return number % 3 === 0 || number % 7 === 0;
+}
+
+console.log(checkMultiple(21));
+console.log(checkMultiple(10));
+console.log(checkMultiple(9));
