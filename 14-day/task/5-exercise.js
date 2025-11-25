0d450475a559b07ecf9e3ac5ad552e54b68edb84
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-25 16:34:41 -0500

    Add function to swap first and last characters of a string

--- 14-day/task/5-exercise.js
+++ 14-day/task/5-exercise.js
@@ -2,3 +2,18 @@
  * Write a JavaScript program to create a new string from a given string by changing the position of the first and last characters.
  * The string length must be broader than or equal to 1.  
  */
+
+function swapFirstAndLastChar(str) {
+    if (str.length < 1) {
+        return "String length must be broader than or equal to 1";
+    }
+
+    const firstChar = str[0];
+    const lastChar = str[str.length - 1];
+    const middleStr = str.slice(1, str.length - 1);
+    return lastChar + middleStr + firstChar;
+}
+
+console.log(swapFirstAndLastChar("Mariana"));
+console.log(swapFirstAndLastChar("A"));
+console.log(swapFirstAndLastChar(""));
