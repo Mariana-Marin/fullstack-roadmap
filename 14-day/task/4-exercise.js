c1cd42f881054758e070e6c7eaec8837a760f118
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-25 16:19:07 -0500

    Add function to remove character from string and log result

--- 14-day/task/4-exercise.js
+++ 14-day/task/4-exercise.js
@@ -2,3 +2,13 @@
  * Write a JavaScript program to remove a character at the specified
  * position in a given string and return the modified string.  
  */
+
+function removeCharacter(str, position) {
+
+    const partOneStr = str.slice(0, position);
+    const partTwoStr = str.slice(position + 1);
+
+    return partOneStr + partTwoStr;
+}
+
+console.log(removeCharacter("Mariana", 3));
