b9c4a1c9b4631c0ac9239f20398c1849870affd7
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-25 15:29:42 -0500

    Add function to calculate absolute difference from 13

--- 14-day/task/0-exercise.js
+++ 14-day/task/0-exercise.js
@@ -3,3 +3,16 @@
  * if the number is broader than 13 return double the absolute difference.  
  */
 
+function getAbsoluteDifference (number) {
+
+    if (number > 13) {
+        number = Math.abs((number-13)*2);
+    } else { 
+        number -= 13;
+    }
+
+    return number;
+}
+
+console.log(getAbsoluteDifference(25));
+console.log(getAbsoluteDifference(13));
