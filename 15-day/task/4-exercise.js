088d6b4b5917425439588d5491635c17cb884787
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-27 12:42:28 -0500

    Add implementation to calculate the area of a triangle using Heron's formula

--- 15-day/task/4-exercise.js
+++ 15-day/task/4-exercise.js
@@ -1,3 +1,15 @@
 /* 4. Calculate Area of Triangle (Sides: 5, 6, 7)
 
 Write a JavaScript program to find the area of a triangle where three sides are 5, 6, 7.  */
+
+//Fórmula de Herón
+//A = √[4a²b² - (a² + b² - c²)²]/4
+
+const a = 5;
+const b = 6;
+const c = 7;
+
+const area =
+  Math.sqrt(4 * a ** 2 * b ** 2 - (a ** 2 + b ** 2 - c ** 2) ** 2) / 4;
+
+console.log(`El área del triángulo es: ${area.toFixed(2)}`);
