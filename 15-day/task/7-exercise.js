72ce37a85b9dfb52ecc988c8ba7c71bf0e33564b
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-27 13:54:15 -0500

    Update exercise descriptions and add implementation for random integer guess game

--- 15-day/task/7-exercise.js
+++ 15-day/task/7-exercise.js
@@ -1,3 +1,5 @@
 /* 7. Find Years When Jan 1 is Sunday (2014?2050)
 
-Write a JavaScript program to find out if 1st January will be a Sunday between 2014 and 2050.  */
+Write a JavaScript program to find out if 1st January will be a Sunday 
+between 2014 and 2050.  */
+
--- 15-day/task/8-exercise.js
+++ 15-day/task/8-exercise.js
@@ -3,3 +3,16 @@
 Write a JavaScript program where the program takes a random integer between 1 and 10, 
 and the user is then prompted to input a guess number. The program displays a 
 message "Good Work" if the input matches the guess number otherwise "Not matched".  */
+
+/*Math.random() → genera número entre 0 y 0.999...
+Math.trunc(0.847362) → elimina decimales */
+
+let secretNumber = Math.trunc(Math.random() * 10) + 1;
+console.log(secretNumber);
+
+// Supongamos que hay un Prompt: "input a guess number"
+let userNumber = 4;
+
+userNumber === secretNumber
+  ? console.log("Good Work")
+  : console.log("Not matched");
