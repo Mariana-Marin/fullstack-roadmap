4b23f98ae138ab812591ad4c216f1c0ab78ead97
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-27 15:48:38 -0500

    Add exercise to get current website URL and create variable with user-defined name

--- 15-day/task/12-exercise.js
+++
@@ -1,3 +0,0 @@
-/*12. Get Current Website URL
-
-Write a JavaScript program to get the website URL (loading page).  */
--- 15-day/task/13-exercise.js
+++ 15-day/task/13-exercise.js
@@ -1,3 +1,4 @@
 /*13. Create Variable with User-Defined Name
 
 Write a JavaScript exercise to create a variable using a user-defined name.  */
+
