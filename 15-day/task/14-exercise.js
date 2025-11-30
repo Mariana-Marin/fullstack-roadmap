58235ac84fb67841f73e25a9f47f0f10c80bf02a
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-29 22:21:01 -0500

    Add exercises for getting file extension and calculating days left before Christmas

--- 15-day/task/14-exercise.js
+++ 15-day/task/14-exercise.js
@@ -1,3 +1,22 @@
 /* 14. Get File Extension of Filename
 
 Write a JavaScript exercise to get the filename extension.  */
+
+function getFileExtension(filename) {
+  let extension = "";
+  let foundDot = false;
+
+  for (let i = filename.length - 1; i >= 0; i--) {
+    if (filename[i] === ".") {
+      foundDot = true;
+      break;
+    }
+    extension = filename[i] + extension;
+  }
+
+  return foundDot ? extension : "";
+}
+
+console.log(getFileExtension("documento.txt"));
+console.log(getFileExtension("noextension"));
+console.log(getFileExtension("foto.jpeg"));
--- 15-day/task/9-exercise.js
+++ 15-day/task/9-exercise.js
@@ -1,3 +1,7 @@
 /*9. Days Left Before Christmas
 
 Write a JavaScript program to calculate the days left before Christmas.  */
+
+let today = new Date();
+let currentYear = today.getFullYear();
+let christmasDate = new Date(currentYear, 11, 25); // mes 11 = diciembre
