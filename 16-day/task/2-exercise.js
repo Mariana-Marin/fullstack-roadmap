da0be6ef17c5b89d4f86ca592a68f1bdb76b89be
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-29 22:30:58 -0500

    Implement getAbsoluteDifference function to compute the absolute difference from 19 and return triple if greater than 19

--- 16-day/task/2-exercise.js
+++ 16-day/task/2-exercise.js
@@ -3,3 +3,17 @@
 // Write a JavaScript program to compute the absolute difference between a 
 // specified number and 19. Returns triple the absolute difference if the 
 // specified number is greater than 19.  
+
+function getAbsoluteDifference(number) {
+  if (number > 19) {
+    number = Math.abs((number - 19) * 3);
+  } else {
+    number = 19 - number;
+  }
+
+  return number;
+}
+
+console.log(getAbsoluteDifference(25));
+console.log(getAbsoluteDifference(19));
+console.log(getAbsoluteDifference(7));
