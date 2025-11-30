3e188bea1b185c0f7a31bcfe5c1c670efcb2813b
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-29 22:23:53 -0500

    Refactor getFileExtension function to improve variable naming and return value for invalid extensions

--- 15-day/task/14-exercise.js
+++ 15-day/task/14-exercise.js
@@ -4,17 +4,17 @@ Write a JavaScript exercise to get the filename extension.  */
 
 function getFileExtension(filename) {
   let extension = "";
-  let foundDot = false;
+  let hasDot = false;
 
   for (let i = filename.length - 1; i >= 0; i--) {
     if (filename[i] === ".") {
-      foundDot = true;
+      hasDot = true;
       break;
     }
     extension = filename[i] + extension;
   }
 
-  return foundDot ? extension : "";
+  return hasDot ? extension : "invalid extension";
 }
 
 console.log(getFileExtension("documento.txt"));
