7dc967eae1ac439767494c0917cb2ec422cfc8ac
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-29 22:27:55 -0500

    Enhance exercise 15 by adding function implementation and test cases for absolute difference calculation

--- 15-day/task/15-exercise.js
+++ 15-day/task/15-exercise.js
@@ -2,3 +2,17 @@
 
 Write a JavaScript program to get the difference between a given number and 13, 
 if the number is broader than 13 return double the absolute difference.  */
+
+function getAbsoluteDifference(number) {
+  if (number > 13) {
+    number = Math.abs((number - 13) * 2);
+  } else {
+    number = 13 - number;
+  }
+
+  return number;
+}
+
+console.log(getAbsoluteDifference(25));
+console.log(getAbsoluteDifference(13));
+console.log(getAbsoluteDifference(7));
