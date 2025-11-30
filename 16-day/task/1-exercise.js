deaf5c2a23502f0794b17ceb98b2a2c51757984c
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-11-29 22:29:56 -0500

    Add implementation and test cases for sumTriple function in exercise 16

--- 16-day/task/1-exercise.js
+++ 16-day/task/1-exercise.js
@@ -1,4 +1,15 @@
 // 16. Sum Two Integers (Triple if Equal)
 
-// Write a JavaScript program to compute the sum of the two given integers. 
-// If the two values are the same, then return triple their sum.  
+// Write a JavaScript program to compute the sum of the two given integers.
+// If the two values are the same, then return triple their sum.
+
+function sumTriple(a, b) {
+  if (a === b) {
+    return (a + b) * 3;
+  } else {
+    return a + b;
+  }
+}
+
+console.log(sumTriple(10, 90));
+console.log(sumTriple(10, 10));
