e389af1fadfbaef28f16c8ec95173e2baacbc184
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 12:22:02 -0500

    Refactor comments and formatting in exercise files for clarity and consistency

--- 17-day/task/1-exercise.js
+++ 17-day/task/1-exercise.js
@@ -1,3 +1,17 @@
 // 31. Find Largest of Three Integers
 
-// Write a JavaScript program to find the largest of three given integers.  
+// Write a JavaScript program to find the largest of three given integers.
+
+function LargestOfThreeIntegers(a, b, c) {
+  // return Math.max(a, b, c);
+  
+  if (a > b && a > c) {
+    return a;
+  } else if (b > a && b > c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+
+console.log(LargestOfThreeIntegers(1, 2, 3));
