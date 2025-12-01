a7bfb2832f444c663646e84036f7cd0ee4303f88
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 10:34:04 -0500

    Add implementation and test cases for isMultipleOf3Or7 function

--- 16-day/task/10-exercise.js
+++ 16-day/task/10-exercise.js
@@ -1,3 +1,11 @@
 // 25. Check if Number is Multiple of 3 or 7
 
-// Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.  
+// Write a JavaScript program to check whether a given positive number is a multiple of 3 or 7.
+
+function isMultipleOf3Or7(number) {
+  return number % 3 === 0 || number % 7 === 0;
+}
+
+console.log(isMultipleOf3Or7(10));
+console.log(isMultipleOf3Or7(14));
+console.log(isMultipleOf3Or7(6));
