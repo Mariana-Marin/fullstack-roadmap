3bd57e096064bec71d0c32acd49692e06cc651b4
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 17:42:01 -0500

    Add ping pong game logic and improve comments for clarity; ensure proper serving rules and game termination conditions

--- 17-day/task/13-exercise.js
+++ 17-day/task/13-exercise.js
@@ -1,4 +1,17 @@
 // 43. Check Rightmost Digits of Three Numbers
 
-// Write a JavaScript program to check from three given 
-// numbers (non negative integers) that two or all of them have the same rightmost digit.  
+// Write a JavaScript program to check from three given
+// numbers (non negative integers) that two or all of them have the same rightmost digit.
+
+function checkLastDigitOfNumber(a, b, c) {
+  return (
+    a.toString().slice(-1) === b.toString().slice(-1) ||
+    b.toString().slice(-1) === c.toString().slice(-1) ||
+    a.toString().slice(-1) === c.toString().slice(-1)
+  );
+}
+
+console.log(checkLastDigitOfNumber(52, 82, 92));
+console.log(checkLastDigitOfNumber(20, 22, 45));
+console.log(checkLastDigitOfNumber(50, 82, 92));
+console.log(checkLastDigitOfNumber(52, 80, 92));
--- 18-day/task/exercisePingPong.js
+++ 18-day/task/exercisePingPong.js
@@ -11,3 +11,27 @@ Crear una función que reciba:
 La función debe retornar:
 - Quién debe sacar en este momento
 - Si el juego ha terminado y quién ganó*/
+
+//Si el residuo es 0 o 1: Saca P1.
+//Si el residuo es 2 o 3: Saca P2.
+
+function pingPongGame(player1, player2, score1, score2) {
+  if (score1 === 11 || score2 === 11) {
+    if (score1 === 11) {
+      return `${player1} is the winner!`;
+    } else {
+      return `${player2} is the winner!`;
+    }
+  }
+
+  if ((score1 + score2) % 4 < 2) {
+    return `It's ${player1}'s turn to serve`;
+  } else {
+    return `It's ${player2}'s turn to serve`;
+  }
+}
+
+console.log(pingPongGame("Mariana", "Dary", 0, 0));
+console.log(pingPongGame("Mariana", "Dary", 0, 1));
+console.log(pingPongGame("Mariana", "Dary", 1, 1));
+console.log(pingPongGame("Mariana", "Dary", 5, 4));
