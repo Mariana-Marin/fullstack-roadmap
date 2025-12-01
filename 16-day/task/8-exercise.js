924096a938c45e2e53e32d1f42615be662489d03
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 10:32:08 -0500

    Add implementations and test cases for string manipulation functions: swapFirstAndLastLetter and addFirstCharToFrontAndBack

--- 16-day/task/8-exercise.js
+++ 16-day/task/8-exercise.js
@@ -3,3 +3,18 @@
 // Write a JavaScript program to create a new string from a given string 
 // by changing the position of the first and last characters.
 // The string length must be broader than or equal to 1.  
+
+function swapFirstAndLastLetter (str) {
+  if (str.length < 1) {
+    return "String length must be broader than or equal to 1";
+  }
+
+  const firstLetter = str[0];
+  const lastLetter = str[str.length - 1];
+  const middleStr = str.slice(1, str.length - 1);
+  return lastLetter + middleStr + firstLetter;
+}
+
+console.log(swapFirstAndLastLetter("Mariana"));
+console.log(swapFirstAndLastLetter("A"));
+console.log(swapFirstAndLastLetter(""));
--- 16-day/task/9-exercise.js
+++ 16-day/task/9-exercise.js
@@ -1,4 +1,16 @@
 // 24. Add First Character to Front and Back of String
 
-// Write a JavaScript program to create another string from a given string 
-// with the first character of the given string added to the front and back.  
+// Write a JavaScript program to create another string from a given string
+// with the first character of the given string added to the front and back.
+
+function addFirstCharToFrontAndBack(str) {
+  if (str.length < 1) {
+    return "String length must be broader than or equal to 1";
+  }
+  const firstLetter = str[0];
+  return firstLetter + str + firstLetter;
+}
+
+console.log(addFirstCharToFrontAndBack("Mariana"));
+console.log(addFirstCharToFrontAndBack("A"));
+console.log(addFirstCharToFrontAndBack(""));
