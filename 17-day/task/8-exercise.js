29f7ff14273013f27eb2ba3194592954272258b0
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 15:43:43 -0500

    Fix formatting and add missing newlines in 8-exercise.js and 9-exercise.js for clarity

--- 17-day/task/8-exercise.js
+++ 17-day/task/8-exercise.js
@@ -6,3 +6,4 @@
 // If the total marks fall within the range of 89 to 100 (inclusive), the student receives an A+ grade.
 // If the examination is labeled as "Final-exam," the student will receive an A+ grade only if their total marks are 90 or greater. Assume that final examination means we pass 'true' as second parameter otherwise blank.
 // If the student achieves an A+ grade, the program should return 'true'; otherwise, it should return 'false'.
+
--- 17-day/task/9-exercise.js
+++ 17-day/task/9-exercise.js
@@ -1,4 +1,15 @@
 // 39. Sum Two Integers and Return Based on Range
 
-// Write a JavaScript program to compute the sum of the two given integers. 
-// If the sum is in the range 50..80 return 65 otherwise return 80.  
+// Write a JavaScript program to compute the sum of the two given integers.
+// If the sum is in the range 50..80 return 65 otherwise return 80.
+
+function sumNumbersAndReturn(a, b) {
+  if (a + b > 50 && a + b < 80) {
+    return 65;
+  } else {
+    return 80;
+  }
+}
+
+console.log(sumNumbersAndReturn(50, 10));
+console.log(sumNumbersAndReturn(80, 10));
