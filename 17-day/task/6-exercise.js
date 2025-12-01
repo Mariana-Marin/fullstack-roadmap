cb453f80ffb66712c767213b544b9da1816b541a
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 15:28:46 -0500

    Refactor checkLastDigitOfNumber function; improve comments and formatting for clarity

--- 17-day/task/6-exercise.js
+++ 17-day/task/6-exercise.js
@@ -1,4 +1,14 @@
 // 36. Check if Last Digit of Three Integers is Same
 
-// Write a JavaScript program that checks whether the last digit of three positive 
-// integers is the same.  
+// Write a JavaScript program that checks whether the last digit of three positive
+// integers is the same.
+
+function checkLastDigitOfNumber(a, b, c) {
+
+  return a.toString().slice(-1) === b.toString().slice(-1) && b.toString().slice(-1) === c.toString().slice(-1);
+
+}
+
+console.log(checkLastDigitOfNumber(52, 82, 92));
+console.log(checkLastDigitOfNumber(5772, 8882, 9882));
+console.log(checkLastDigitOfNumber(50, 82, 92));
