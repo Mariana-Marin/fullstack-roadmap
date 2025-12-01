7c92fcdcb5ae7d4c5c1da18918e6df02e5774bab
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 15:54:26 -0500

    Refactor checkSameNumbers function; improve formatting and add missing newlines for clarity

--- 17-day/task/11-exercise.js
+++ 17-day/task/11-exercise.js
@@ -3,3 +3,18 @@
 // Write a JavaScript program to check a set of three numbers; 
 // if the three numbers are the same return 30; otherwise return 20; 
 // and if two numbers are the same return 40.  
+
+function checkSameNumbers (a, b, c){
+
+    if (a === b && b === c){
+        return 30;
+    } else if (a === b || b === c || a === c){
+        return 40;
+    } else {
+        return 20
+    }
+}
+
+console.log(checkSameNumbers(2,2,2));
+console.log(checkSameNumbers(2,3,2));
+console.log(checkSameNumbers(1,2,3));
