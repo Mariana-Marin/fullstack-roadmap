a13653cbe7fbd36f71b0934b3e936db6631a001c
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-01 11:27:17 -0500

    Implement addLast3CharsToFrontAndBack function with input validation and example usage

--- 16-day/task/11-exercise.js
+++ 16-day/task/11-exercise.js
@@ -3,3 +3,16 @@
 // Write a JavaScript program to create a string from a given string. 
 // This is done by taking the last 3 characters and adding them at both the front and back.
 // The string length must be 3 or more.  
+
+function addLast3CharsToFrontAndBack(str) {
+  if (str.length < 3) {
+    return "String length must be 3 or more";
+  }
+  const last3Chars = str[str.length - 3] + str[str.length - 2] + str[str.length - 1];
+  // Alternatively: const last3Chars = str.slice(-3);
+  return last3Chars + str + last3Chars;
+}
+
+console.log(addLast3CharsToFrontAndBack("Mariana"));
+console.log(addLast3CharsToFrontAndBack("abc"));
+console.log(addLast3CharsToFrontAndBack("hi"));
