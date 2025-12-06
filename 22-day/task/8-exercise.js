f89ae3860e1cc7bfb65d7298d2e18d967af1f022
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-06 17:33:01 -0500

    Refactor sumSeries function; improve formatting and add missing newline at end of file.

--- 22-day/task/8-exercise.js
+++ 22-day/task/8-exercise.js
@@ -1,3 +1,15 @@
 ﻿// 113. Sum n + n/2 + n/4 + ...
 // 
 // Write a JavaScript program to calculate the sum of n + n/2 + n/4 + n/8 + .... where n is a positive integer and all divisions are integers. 
+
+function sumSeries(n) {
+  let sum = 0;
+    while (n >= 1) {
+        sum += n;
+        n = Math.floor(n / 2);
+    }
+    return sum;
+}
+console.log(sumSeries(10));
+console.log(sumSeries(25));
+console.log(sumSeries(50));
