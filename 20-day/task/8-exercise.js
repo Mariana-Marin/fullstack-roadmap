df80785e9f91e0e3a759a9a47b8b52a5c30f345d
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-06 15:14:38 -0500

    Refactor findLongestString exercise; improve code structure and add example test cases

--- 20-day/task/8-exercise.js
+++ 20-day/task/8-exercise.js
@@ -1,3 +1,22 @@
 ﻿// 83. Find Longest String in Array
-// 
-// Write a JavaScript program to find the longest string from a given array of strings.  
+//
+// Write a JavaScript program to find the longest string from a given array of strings.
+
+function findLongestString(array) {
+  let maxLength = 0;
+  let longestString = "";
+  
+  for (const element of array) {
+    if (element.length > maxLength) {
+      maxLength = element.length;
+      longestString = element;
+    }
+  }
+
+  return longestString;
+}
+
+console.log(findLongestString(["a", "aa", "aaa"]));
+console.log(findLongestString(["aaaa", "aa", "aaa"]));
+console.log(findLongestString(["aa", "aa", "aa"]));
+console.log(findLongestString(["aa", "aaa", "aa"]));
