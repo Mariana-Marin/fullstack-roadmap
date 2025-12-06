a23f7bb1691c72f33077ee3c3d2e7bc90c217586
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-06 17:31:36 -0500

    Refactor maxIntegerByRemovingOneDigit function; remove implementation and console logs for clarity.

--- 21-day/task/13-exercise.js
+++ 21-day/task/13-exercise.js
@@ -3,16 +3,3 @@
 // Write a JavaScript program to find the maximum number of
 //  a given positive integer by deleting exactly one digit
 // of the given number.
-
-function maxIntegerByRemovingOneDigit(num) {
-  const numStr = num.toString();
-  let maxNum = 0;
-
-  for (let index = 0; index < numStr.length; index++) {
-    const newNum = Number(numStr.slice(0, index) + numStr.slice(index + 1));
-    maxNum = Math.max(maxNum, newNum);
-  }
-  return maxNum;
-}
-console.log(maxIntegerByRemovingOneDigit(152));
-console.log(maxIntegerByRemovingOneDigit(1001));
