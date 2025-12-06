68494094e45b43bb152ab8f0f53331ab8c0a6f53
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-06 12:34:07 -0500

    Refactor comments for clarity and improve function structure in takeMiddleElement

--- 19-day/task/15-exercise.js
+++ 19-day/task/15-exercise.js
@@ -1,3 +1,19 @@
 ﻿// 75. Create Array with Middle Elements from Two Arrays
-// 
-// Write a JavaScript program to create an array taking the middle elements of the two arrays of integer and each length 3.  
+//
+// Write a JavaScript program to create an array
+// taking the middle elements of the two arrays
+// of integer and each length 3.
+
+function takeMiddleElement(array1, array2) {
+  const newArr = [];
+  newArr.push(array1[1]);
+  newArr.push(array2[1]);
+
+  //newArr.push(array1[1], array2[1]);
+
+  return newArr;
+
+  //return [array1[1], array2[1]];
+}
+
+console.log(takeMiddleElement([1, 2, 3], [1, 5, 6]));
