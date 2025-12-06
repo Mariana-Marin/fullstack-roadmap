62af17894d09ebbda40133448dc777424c1603e3
Mariana Marin <marianamarinvanegas2014@gmail.com>
2025-12-06 16:46:45 -0500

    Refactor findKthGreatest function; improve formatting and add missing newline at end of file

--- 20-day/task/15-exercise.js
+++ 20-day/task/15-exercise.js
@@ -1,12 +1,17 @@
 ﻿// 90. Find kth Greatest Element in Array
-// 
-// Write a JavaScript program to find the kth greatest element in a given array of integers.  
+//
+// Write a JavaScript program to find the kth greatest element in a given array of integers.
 
 // "kth greatest" significa el k-ésimo elemento más grande del array.
 
-function findKthGreatest (array, kth){
+function findKthGreatest(array, kth) {
+  array.sort(function (a, b) {
+    return b - a;
+  });
 
-    
-
-
+  return array[kth - 1];
 }
+
+console.log(findKthGreatest([1, 3, 4, 5, 6], 2));
+console.log(findKthGreatest([10, 4, 3, 50, 23, 90], 1));
+console.log(findKthGreatest([5, 2, 9, 1, 7], 1));
